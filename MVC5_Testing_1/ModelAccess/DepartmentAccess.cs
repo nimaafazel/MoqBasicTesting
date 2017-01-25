951eedda823574a8cfcf14651e44dafeaa261f85
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2017-01-25 10:31:37 -0800

    First commit

---
+++ MVC5_Testing_1/ModelAccess/DepartmentAccess.cs
@@ -0,0 +1,82 @@
+﻿using MVC5_Testing_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_Testing_1.ModelAccess
+{
+    /// <summary>
+    /// Interface to provide functionality for Department Data Access
+    /// </summary>
+    public interface IDepartmentAccess
+    {
+        List<Department> GetDepartments();
+        Department GetDepartment(int id);
+        void CreateDepartment(Department dept);
+        bool CheckDepartmentExists(int id);
+    }
+
+    /// <summary>
+    /// The Department Data Access
+    /// </summary>
+    public class DepartmentAccess : IDepartmentAccess
+    {
+        CompanyEntities ctx;
+
+        public DepartmentAccess()
+        {
+            ctx = new CompanyEntities();
+        }
+
+        /// <summary>
+        /// Gets all the Departments
+        /// </summary>
+        /// <returns></returns>
+        public List<Department> GetDepartments()
+        {
+            var depts = ctx.Departments.ToList();
+            return depts;
+        }
+
+        /// <summary>
+        /// Gets department based on id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Department GetDepartment(int id)
+        {
+            //var dept = ctx.Departments.FirstOrDefault(d => d.DeptNo == id);
+            var dept = ctx.Departments.Find(id);
+            return dept;
+        }
+
+        /// <summary>
+        /// Adds a Department to the Departments list
+        /// </summary>
+        /// <param name="dept"></param>
+        public void CreateDepartment(Department dept)
+        {
+            ctx.Departments.Add(dept);
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Checks if the Deparment exists or not.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool CheckDepartmentExists(int id)
+        {
+            //var dep = ctx.Departments.FirstOrDefault(d => d.DeptNo == id);
+            var dept = ctx.Departments.Find(id);
+            return dept != null ? true : false;
+        }
+
+        
+
+        
+
+        
+    }
+}
---
+++ MVCModelTest/CheckDepartmentTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using MVC5_Testing_1.ModelAccess;
+using Moq;
+using System.Collections.Generic;
+
+namespace MVCModelTest
+{
+    [TestFixture]
+    public class CheckDepartmentTest
+    {
+        //[Test]
+        //public void CheckDepartmentExists()
+        //{
+        //    var obj = new DepartmentAccess();
+        //    var res = obj.CheckDepartmentExists(10);
+        //    Assert.That(res, Is.True);
+        //}
+
+        [Test]
+        public void CheckDepartmentExistsWithMoq()
+        {
+            // Create fake object
+            var fakeObject = new Mock<IDepartmentAccess>();
+
+            // Set the mock configuration
+            // The CheckDeptExists() method call is set with an Integer type parameter
+            // The configuration also defines the return type for the method
+            fakeObject.Setup(x => x.CheckDepartmentExists(It.IsAny<int>())).Returns(true);
+
+
+            // call the method
+            var res = fakeObject.Object.CheckDepartmentExists(10);
+
+            Assert.That(res, Is.True);
+        }
+    }
+}
